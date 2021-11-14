99dfc2a47858f9f8db965cfe38dfe8b518533e29
TX <tx@Macbooks-MacBook-Pro.local>
2021-11-14 14:14:28 +0900

    remove PWA

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 
-module.exports = withPWA({
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+// });
+module.exports = {
   reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
-});
+};
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -44,6 +44,7 @@ export default function Login() {
   const [loginSuccess, setLoginSuccess] = useState(false);
   const router = useRouter();
   const dispatch = useAuthDispatch();
+
   const { authenticated } = useAuthState();
 
   useEffect(() => {
